784f377a3f6a14c44ca09cc9a67ab3747a37e630
JuanFila <juanfilapablo@hotmail.com>
2024-03-28 23:33:22 -0300

    add post

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header/Header';
 import styles from './App.module.css'
 import './styles/global.css';
 import { SideBar } from './components/sidebar/SideBar';
+import { Post } from './components/Post/Post';
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
       <div className={styles.wrapper}>
         <SideBar/>
         <main>
-          POSTS
+          <Post/>
+          <Post/>
         </main>
       </div>
     </div>
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,34 @@
+import style from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={style.post}>
+      <header>
+        <div className={style.author}>
+          <img src="https://github.com/JuanFila.png" className={style.avatar} />
+          <div className={style.authorInfo}>
+            <strong>Juan Fila</strong>
+            <span>Front-End Web Developer</span>
+          </div>
+        </div>
+
+        <time title="28 de março às 07:12" dateTime="2024-02-29 07:12:40">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={style.content}>
+        <p>Fala galeraa 👋 </p>
+        
+       <p> Acabei de subir mais um projeto no meu portfolio. É um
+        projeto que fiz sobre um feed de postagens de usuário... É somente o front
+        mas é um 🚀 </p>
+        
+        <p>
+          <a href="#">#novoprojeto </a>
+          <a href="#">#FeedComReact </a>
+        </p>
+      </div>
+    </article>
+  );
+}
