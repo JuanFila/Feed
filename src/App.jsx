130e03074fc2d45c35feb1b48666fc2632d28beb
JuanFila <juanfilapablo@hotmail.com>
2024-04-08 00:56:51 -0300

    update info

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,41 @@ import './styles/global.css';
 import { SideBar } from './components/sidebar/SideBar';
 import { Post } from './components/Post/Post';
 
+// author {avatar_url: "", name: "",, role ""}
+// publisheAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      authorUrl: 'https://github.com/JuanFila.png',
+      name: 'Juan Fila',
+      role: 'Dev-FRONT'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋 '},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz sobre um feed de postagens de usuário... É somente o front mas é um 🚀'},
+      {type: 'link', content: 'novoprojeto'},       
+    ],
+    publishedAt: new Date('2024-04-01') 
+  },
+  {
+    id: 2,
+    author: {
+      authorUrl: 'https://github.com/Diego3g.png',
+      name: 'Diego',
+      role: 'CEO'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋 '},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz sobre um feed de postagens de usuário... É somente o front mas é um 🚀'},
+      {type: 'link', content: 'novoprojeto'},       
+    ],
+    publishedAt: new Date('2024-05-03') 
+  },
+]
+
 export default function App() {
   return (
     <div>
@@ -12,8 +47,16 @@ export default function App() {
       <div className={styles.wrapper}>
         <SideBar/>
         <main>
-          <Post/>
-          <Post/>
+         {posts.map(post=> {
+           return (
+           <Post
+           key={post.id}
+           author={post.author}
+           content={post.content}
+           publishedAt={post.publishedAt}
+           />
+          )
+         })}
         </main>
       </div>
     </div>
