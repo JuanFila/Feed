4d636845c36acd23679ccacded68f7de6c6f4e63
JuanFila <juanfilapablo@hotmail.com>
2024-04-10 22:10:46 -0300

    update

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { Avatar } from "../Avatar/avatar";
 import style from "./Comment.module.css";
 import {Trash, ThumbsUp} from "phosphor-react"
 
 export function Comment({content, OnDeleteComment, publishedAt}) {
+  
+  const [likeCount, setLikeCount] = useState(0);
+  
   function handleDeleteComment() {
     OnDeleteComment(content) // o melhor dos casos seria utilziar o id
+  }
+
+  function handleLikeComment() { // validação sem back-end
+     if(likeCount === 0){
+       setLikeCount(likeCount + 1)
+    } else{
+      setLikeCount(0)
+    }
   }
   return (
     <div className={style.comment}>
@@ -28,9 +40,9 @@ export function Comment({content, OnDeleteComment, publishedAt}) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp/>
-            Aplaudir <span>20</span>
+            Gostei <span>{likeCount}</span>
           </button>
         </footer>
       </div>
